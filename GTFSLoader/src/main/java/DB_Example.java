70c454824f55b80d97dbc6495a571d60821dc861
Joan Guisado-Gámez <joan@dync-34-139.ac.upc.es>
2018-04-04 16:35:07 +0200

    -f option to pass a single file, -F option to passa a folder with multiple GTFS files

--- GTFSLoader/src/main/java/DB_Example.java
+++ GTFSLoader/src/main/java/DB_Example.java
@@ -3,7 +3,9 @@ import MySQLHandlers.SQLConnection;
 import MySQLHandlers.SQLQueryBuilder;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -27,6 +29,13 @@ public class DB_Example {
         @Parameter(names = {"-pw", "--password"}, description = "DB password", required = false)
         public String pw = "confluent";
 
+        @Parameter(names={"-f","--file"}, description="Full path to the GTFS file to be loaded", required= false)
+        public String filename="";
+
+
+        @Parameter(names={"-F","--folder"}, description="Full path to the folder containing the GTFS files to be loaded", required= false)
+        public String folderName="";
+
         @Parameter(names = "--help,-help", help = true)
         private boolean help;
 
@@ -35,11 +44,22 @@ public class DB_Example {
 
     public static void main (String[] args) throws IOException, SQLException {
 
-        System.out.println("Number of arguments " + args.length);
 
         Arguments arguments = new Arguments();
-        JCommander.newBuilder().addObject(arguments).build().parse(args);
+        try{
 
+            JCommander.newBuilder().addObject(arguments).build().parse(args);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        if(arguments.filename.isEmpty()&&arguments.folderName.isEmpty())
+        {
+            System.out.println("Either the filename [-f] or the folder [-F] containing the files to be loaded has to be passed as an argument.");
+        }
         //new JCommander (arguments,args);
 
 
@@ -48,22 +68,46 @@ public class DB_Example {
         String user = arguments.user;
         String pw = arguments.pw;
 
-        //final Connection connection = SQLConnection.getConnection("192.168.99.100", "connect_test", "confluent", "confluent");
-        final Connection connection = SQLConnection.getConnection(ip,database,user,pw);
+        //connection = SQLConnection.getConnection("192.168.99.100", "connect_test", "confluent", "confluent");
+     //   connection = SQLConnection.getConnection(ip,database,user,pw);
 
 
-        List<String[]> records = LoadFile.load("/Users/joan/Downloads/original_gtfs/routes.txt");
-        String createTable = SQLQueryBuilder.createTable("routes", records.get(0));
+        if(!arguments.filename.isEmpty())
+        {
+           processFile(arguments.filename);
+        }
+        if(!arguments.folderName.isEmpty())
+        {
+            File folder = new File(arguments.folderName);
+            String[] listOfFiles = ArrayUtils.removeElement(folder.list(),arguments.filename);
+            for(String filename : listOfFiles)
+            {
+                if (!filename.startsWith(".")) {
+                    System.out.println("Processing file: " + arguments.folderName + "/" + filename);
+                    processFile(arguments.folderName + "/" + filename);
+                }
+            }
+        }
+    }
+
+static    Connection connection;
+
+    private static void processFile(String filename) throws IOException, SQLException {
+        String tableName = filename.substring(filename.lastIndexOf("/")+1,filename.lastIndexOf("."));
+        List<String[]> records = LoadFile.load(filename);
+        String createTable = SQLQueryBuilder.createTable(tableName, records.get(0));
 
         System.out.println(createTable);
 
         Statement statement = connection.createStatement();
-        statement.executeUpdate(createTable);
+       statement.executeUpdate(createTable);
+
+
 
         boolean first = true;
         for(String[] record : records) {
             if (first != true) {
-                String insert = SQLQueryBuilder.insert("routes", records.get(0), record);
+                String insert = SQLQueryBuilder.insert(tableName, records.get(0), record);
                 try {
                     statement.executeUpdate(insert);
                 }
