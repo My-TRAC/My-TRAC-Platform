aea3961298ddc6f7b4a59e91d28bd4bd468d2864
Joan Guisado-Gámez <joan@joan-2.local>
2018-04-12 16:19:34 +0200

    Update

--- GTFSLoader/src/main/java/DB_Example.java
+++ GTFSLoader/src/main/java/DB_Example.java
@@ -3,14 +3,22 @@ import MySQLHandlers.SQLConnection;
 import MySQLHandlers.SQLQueryBuilder;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
-import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.Part;
+import java.io.*;
+import java.nio.file.*;
+import static spark.Spark.*;
+
 
 public class DB_Example {
 
@@ -42,8 +50,81 @@ public class DB_Example {
     }
 
 
+    static    Connection connection;
     public static void main (String[] args) throws IOException, SQLException {
 
+
+        File uploadDir = new File("upload");
+        uploadDir.mkdir(); // create the upload directory if it doesn't exist
+
+        staticFiles.externalLocation("upload");
+
+
+        Arguments arguments = new Arguments();
+
+
+        JCommander.newBuilder().addObject(arguments).build().parse(args);
+        String ip= arguments.ip;
+        String database = arguments.database;
+        String user = arguments.user;
+        String pw = arguments.pw;
+
+     /*   ip="192.168.99.100";
+        database="connect_test";
+    */
+
+       System.out.println(
+              "ip: "+ip+"\n"+
+              "database: "+database+"\n"+
+              "user: "+user+"\n"+
+              "pw: "+pw+"\n"
+       );
+
+        connection = SQLConnection.getConnection(ip,database,user,pw);
+
+        get("/", (req, res) ->
+                "<form method='post' enctype='multipart/form-data'>" // note the enctype
+                        + "    <input type='file' name='uploaded_file' accept='.txt'>" // make sure to call getPart using the same "name" in the post
+                        + "    <button>Upload GTFS file</button>"
+                        + "</form>"
+        );
+
+        post("/", (req, res) -> {
+
+            Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
+
+            req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+
+            String rawFilename="";
+            try (InputStream input = req.raw().getPart("uploaded_file").getInputStream()) { // getPart needs to use same "name" as input field in form
+                Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+                rawFilename=getFileName(req.raw().getPart("uploaded_file"));
+            }
+
+            catch (Exception e )
+            {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+
+
+            //logInfo(req, tempFile);
+            File f = tempFile.toFile();
+
+
+
+
+
+            String result = processFile(rawFilename,f.getAbsolutePath());
+
+
+            Files.delete(tempFile);
+            return "<h1>Results:</h1>"+result;
+        });
+
+
+
+/*
 
         Arguments arguments = new Arguments();
         try{
@@ -60,7 +141,6 @@ public class DB_Example {
         {
             System.out.println("Either the filename [-f] or the folder [-F] containing the files to be loaded has to be passed as an argument.");
         }
-        //new JCommander (arguments,args);
 
 
         String ip= arguments.ip;
@@ -68,13 +148,12 @@ public class DB_Example {
         String user = arguments.user;
         String pw = arguments.pw;
 
-        //connection = SQLConnection.getConnection("192.168.99.100", "connect_test", "confluent", "confluent");
-     //   connection = SQLConnection.getConnection(ip,database,user,pw);
+        connection = SQLConnection.getConnection(ip,database,user,pw);
 
 
         if(!arguments.filename.isEmpty())
         {
-           processFile(arguments.filename);
+            processFile(arguments.filename);
         }
         if(!arguments.folderName.isEmpty())
         {
@@ -88,19 +167,36 @@ public class DB_Example {
                 }
             }
         }
+        */
     }
 
-static    Connection connection;
+    private static String getFileName(Part part) {
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
 
-    private static void processFile(String filename) throws IOException, SQLException {
-        String tableName = filename.substring(filename.lastIndexOf("/")+1,filename.lastIndexOf("."));
+
+    private static String processFile(String rawFilename, String filename) throws IOException, SQLException {
+
+        System.out.println("File: "+filename);
+
+        String tableName = rawFilename.substring(rawFilename.lastIndexOf("/")+1,rawFilename.lastIndexOf("."));
+
+        System.out.println("Table name: "+tableName);
         List<String[]> records = LoadFile.load(filename);
         String createTable = SQLQueryBuilder.createTable(tableName, records.get(0));
 
         System.out.println(createTable);
 
         Statement statement = connection.createStatement();
-       statement.executeUpdate(createTable);
+        statement.executeUpdate(createTable);
+
+        StringBuilder result = new StringBuilder("Table:<br>"+createTable.replace("\n","<br>")+"<br><br>Inserts:<br>");
+        StringBuilder errors = new StringBuilder();
 
 
 
@@ -110,14 +206,17 @@ static    Connection connection;
                 String insert = SQLQueryBuilder.insert(tableName, records.get(0), record);
                 try {
                     statement.executeUpdate(insert);
+                    result.append(insert+"<br>");
                 }
                 catch (Exception e)
                 {
-                    System.out.println("Error in: "+ insert);
+                    System.out.println("Error in "+ insert);
+                    errors.append(insert+"<br>");
                 }
             }
             first = false;
         }
-
+        if(!errors.toString().isEmpty()) result.append("<br><br>Errors:<br>").append(errors);
+        return result.toString();
     }
 }
