8c4e4cd5003f325d0ebcf24c3f647ff6216a2fdf
joangui <joan@ac.upc.edu>
2018-05-18 12:43:21 +0200

    Refactoring

--- cigo-compose/scripts/setJDBCConnector.sh
+++ cigo-compose/scripts/setJDBCConnector.sh
@@ -9,9 +9,15 @@ echo "Adding JDBC connector to http://$CONNECT_HOST:28083/connectors"
 echo ""
 echo ""
 
+#curl -X POST \
+#  -H "Content-Type: application/json" \
+#  --data '{ "name": "cigo-jdbc-source", "config": { "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector", "tasks.max": 1, "connection.url": "jdbc:mysql://127.0.0.1:3306/connect_test?user=confluent&password=confluent", "mode": "incrementing", "incrementing.column.name": "id", "timestamp.column.name": "modified", "topic.prefix": "cigo-jdbc-", "poll.interval.ms": 1000 } }' \
+#  http://$CONNECT_HOST:28083/connectors
+
+
 curl -X POST \
   -H "Content-Type: application/json" \
-  --data '{ "name": "cigo-jdbc-source", "config": { "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector", "tasks.max": 1, "connection.url": "jdbc:mysql://127.0.0.1:3306/connect_test?user=confluent&password=confluent", "mode": "incrementing", "incrementing.column.name": "id", "timestamp.column.name": "modified", "topic.prefix": "cigo-jdbc-", "poll.interval.ms": 1000 } }' \
+  --data '{ "name": "cigo-jdbc-source", "config": { "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector", "tasks.max": 1, "connection.url": "jdbc:mysql://mysql:3306/connect_test?user=confluent&password=confluent", "mode": "incrementing", "incrementing.column.name": "id", "timestamp.column.name": "modified", "topic.prefix": "cigo-jdbc-", "poll.interval.ms": 1000 } }' \
   http://$CONNECT_HOST:28083/connectors
 
   echo ""
