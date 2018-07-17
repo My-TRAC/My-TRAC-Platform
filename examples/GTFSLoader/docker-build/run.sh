61898a616403060ec7f06bf86aa1f783b2bbd0c7
joangui <joan@ac.upc.edu>
2018-07-17 14:42:17 +0200

    run.sh modified to call waitforkafka

--- examples/GTFSLoader/docker-build/run.sh
+++ examples/GTFSLoader/docker-build/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-setJDBCConnector.sh
+sleep 180
+/opt/GTFSLoader/scripts/waitForKafkaConenct.sh
+/opt/GTFSLoader/scripts/setJDBCConnector.sh
 java -jar /opt/GTFSLoader/GTFSLoader-1.0-SNAPSHOT-jar-with-dependencies.jar -db connect_test -ip $MYSQL_HOST 
