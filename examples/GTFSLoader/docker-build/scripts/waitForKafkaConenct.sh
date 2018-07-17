604ec96b309a70766c2a9c73dc668e30b608a0b0
joangui <joan@ac.upc.edu>
2018-07-17 15:27:06 +0200

    Script for waitinf kafka fixed...again

--- examples/GTFSLoader/docker-build/scripts/waitForKafkaConenct.sh
+++ examples/GTFSLoader/docker-build/scripts/waitForKafkaConenct.sh
@@ -3,7 +3,7 @@
 printf "%s" "waiting for Kafka-Connect ..."
 while ! ping -c 1 -n -w 1 kafka-connect &> /dev/null
 do
-	sleep(1)
+	sleep 1
     printf "%c" "Trying to connect to kafka-connect"
 done
 printf "\n%s\n"  "Kafka-Connect is online"
