96243652afe6f54baba8f84526e7d3c356e891c0
joangui <joan@ac.upc.edu>
2018-07-20 11:48:54 +0200

    New scripts for loading gtfsloader in order to create the elastic-search connector

--- examples/GTFSLoader/docker-build/run.sh
+++ examples/GTFSLoader/docker-build/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 180
 /opt/GTFSLoader/scripts/waitForKafkaConenct.sh
 /opt/GTFSLoader/scripts/setJDBCConnector.sh
+/opt/GTFSLoader/scripts/setElasticSearchConnector.sh
 java -jar /opt/GTFSLoader/GTFSLoader-1.0-SNAPSHOT-jar-with-dependencies.jar -db connect_test -ip $MYSQL_HOST 
