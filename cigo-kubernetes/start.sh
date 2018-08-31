302e701634f0928cdc8ab5e30c35b3b24796ab98
ArnauPrat <arnau.prat@gmail.com>
2018-08-31 09:57:46 +0200

    Updated kubernetes deployment to allow for external connections to the cluster

--- cigo-kubernetes/start.sh
+++ cigo-kubernetes/start.sh
@@ -1,22 +1,27 @@
 #/bin/bash
 
-
 #Deploys Zookeeper
 kubectl apply -f cigo-zookeeper-deployment.yaml
-sleep 30
+sleep 5
 
 #Deploys Kafka
 kubectl apply -f cigo-kafka-deployment.yaml
-sleep 30
+sleep 5
 
 #Deploys SchemaRegistry 
 kubectl apply -f cigo-schema-registry-deployment.yaml
-sleep 30
+sleep 5
 
+#Deploys Kafka Connect
 kubectl apply -f cigo-kafka-connect-deployment.yaml
-sleep 30
+sleep 5
 
-kubectl get statefulsets
-kubectl get deployments 
-kubectl get services 
-kubectl get pods
+#Deploys Elasticsearch
+kubectl apply -f cigo-elasticsearch-deployment.yaml
+sleep 5
+
+#Deploys Kibana
+kubectl apply -f cigo-kibana-deployment.yaml
+sleep 5
+
+kubectl get all
