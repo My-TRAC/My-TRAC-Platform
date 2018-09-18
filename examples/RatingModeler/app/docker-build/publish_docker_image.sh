c9abf662e0d1a1bd0fbc2db733d339ba2b11cb49
joangui <joan@ac.upc.edu>
2018-09-18 14:44:23 +0200

    .

--- examples/RatingModeler/app/docker-build/publish_docker_image.sh
+++ examples/RatingModeler/app/docker-build/publish_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ../app/target/RatingModeler-1.0-SNAPSHOT-jar-with-dependencies.jar .
+cp ../target/RatingModeler-1.0-SNAPSHOT-jar-with-dependencies.jar .
 docker build -t sparsitytechnologies/ratingmodeler:latest .
 docker push sparsitytechnologies/ratingmodeler:latest
 
