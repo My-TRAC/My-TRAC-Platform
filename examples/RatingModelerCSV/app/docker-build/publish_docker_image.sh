60110868516f58a214740e6a0b634dbae227853f
joangui <joan@ac.upc.edu>
2018-10-16 11:28:34 +0200

    Refractoring structure

--- examples/RatingModelerCSV/app/docker-build/publish_docker_image.sh
+++ examples/RatingModelerCSV/app/docker-build/publish_docker_image.sh
@@ -13,10 +13,12 @@ then
     eval $(docker-machine env handsOn)
     docker network create handsOnNet
 fi
-
+ 
+cp  -r ../src/test/resources .
 cp ../target/RatingModelerCSV-1.0-SNAPSHOT-jar-with-dependencies.jar .
 docker build -t ratingmodelercsv:latest .
 #docker push sparsitytechnologies/ratingmodelerCSV:latest
 rm RatingModelerCSV-1.0-SNAPSHOT-jar-with-dependencies.jar
 rm -rf ../target
+rm -rf resources
 
