bf7648defac6849b0e21ea69c52f62a5fd0a9017
joangui <joan@ac.upc.edu>
2018-10-17 14:32:25 +0200

    .

--- examples/RatingModeler/app/docker-build/run.sh
+++ examples/RatingModeler/app/docker-build/run.sh
@@ -5,5 +5,5 @@
 /opt/RatingModeler/scripts/waitForMySQL.sh
 /opt/RatingModeler/scripts/setJDBCConnector.sh
 /opt/RatingModeler/scripts/setJDBCSinkConnector.sh
-/opt/RatingModeler/scripts/setElasticSearchConnector.sh
+#/opt/RatingModeler/scripts/setElasticSearchConnector.sh
 java -jar /opt/RatingModeler/RatingModeler-1.0-SNAPSHOT-jar-with-dependencies.jar -db connect_test -ip $MYSQL_HOST 
