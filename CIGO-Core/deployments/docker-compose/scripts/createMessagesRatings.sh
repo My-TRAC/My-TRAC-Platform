cb9a3cd8647de72337d94be1fc4ee9fc1f869eae
joangui <joan@ac.upc.edu>
2019-01-30 15:46:57 +0100

    Dubugging

---
+++ CIGO-Core/deployments/docker-compose/scripts/createMessagesRatings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+eval $(docker-machine env cigo)
+docker run -it --net=cigo-compose_default --rm sparsitytechnologies/cigo-schema-registry kafka-avro-console-producer -broker-list kafka:29092 --topic ratings  --property schema.registry.url="http://schema-registry:8081" --property value.schema='{"type":"record","name":"ratings_schema","fields":[{"name":"user_id","type":"int"},{"name":"activity_id", "type": "int"}, {"name":"rating", "type": "double"}]}'
+
+
+
+#Example of messages:
+#{"user_id":1,"activity_id":33,"rating":2.4}
