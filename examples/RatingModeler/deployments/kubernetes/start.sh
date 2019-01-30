349211eb7bf961806695ee1d1bef5d049b3f8764
joangui <joan@ac.upc.edu>
2019-01-30 15:45:24 +0100

    RatingModeler kubernetes deployment created

--- examples/RatingModeler/deployments/kubernetes/start.sh
+++ examples/RatingModeler/deployments/kubernetes/start.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
+echo "Deployin Rating Modeler..."
+
 kubectl apply -f mysql-deployment.yaml 
 sleep 60
 
-kubectl apply -f gtfsloader-deployment.yaml
+kubectl apply -f ratingmodeler-deployment3.yaml
 
--- examples/RatingModeler/deployments/kubernetes/stop.sh
+++ examples/RatingModeler/deployments/kubernetes/stop.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 kubectl delete -f mysql-deployment.yaml 
-kubectl delete -f gtfsloader-deployment.yaml 
+kubectl delete -f ratingmodeler-deployment3.yaml 
