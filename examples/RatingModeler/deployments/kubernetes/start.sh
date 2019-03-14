406fc6f8f4d635bea442fe5b146124062aba2351
joangui <joan@ac.upc.edu>
2019-03-14 16:16:52 +0100

    Refractoring

--- examples/RatingModeler/deployments/kubernetes/start.sh
+++ examples/RatingModeler/deployments/kubernetes/start.sh
@@ -5,5 +5,5 @@ echo "Deployin Rating Modeler..."
 kubectl apply -f mysql-deployment.yaml 
 sleep 60
 
-kubectl apply -f ratingmodeler-deployment3.yaml
+kubectl apply -f ratingmodeler-deployment.yaml
 
