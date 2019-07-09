32e65273dc6230bbb3aaed2bb7be0a8db7491c66
joangui <joan@ac.upc.edu>
2019-07-09 14:56:09 +0200

    Print kafka key option added

--- deployments/docker-compose/scripts/checkTopic.sh
+++ deployments/docker-compose/scripts/checkTopic.sh
@@ -5,4 +5,4 @@ then
 eval $(docker-machine env cigo)                                                  
 fi
 
-docker run --net=cigo-compose_default --rm sparsitytechnologies/cigo-schema-registry kafka-avro-console-consumer --bootstrap-server kafka:29092 --topic $1 --new-consumer --from-beginning --property schema.registry.url="http://schema-registry:8081"
+docker run --net=cigo-compose_default --rm sparsitytechnologies/cigo-schema-registry kafka-avro-console-consumer --bootstrap-server kafka:29092 --topic $1 --new-consumer --from-beginning --property schema.registry.url="http://schema-registry:8081"  --property print.key=true
